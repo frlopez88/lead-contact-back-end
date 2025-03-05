541b1d42b972a1559c78306b38dad574cd1330d8
frlopez88 <frlopez88@icloud.com>
2025-03-04 19:59:23 -0600

    Cors Problem

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express'
 const app = express()
 import { lead } from './routes/leadRoute.js'
 import {config} from 'dotenv'
+import cors from 'cors'
 
 config()
 const port = process.env.PORT || 8080
 
+app.use(cors())
 app.use(express.json())
 app.use('/leads',lead)
 
